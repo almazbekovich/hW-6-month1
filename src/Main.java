5924aabbdf85b8d05d22ab3e944c4a827abdc471
almazbekovich <120068058+almazbekovich@users.noreply.github.com>
2023-10-18 23:36:18 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Создаем экземпляр оружия
+        
         Weapon bossWeapon = new Weapon();
         bossWeapon.setType(WeaponType.SKS);
         bossWeapon.setName("Doombringer");
-
-        // Создаем экземпляр босса и задаем свойства
         Boss boss = new Boss();
         boss.setName("Evil Boss");
         boss.setHealth(1000);
         boss.setWeapon(bossWeapon);
-
-        // Выводим информацию о боссе
+        
         System.out.println("Boss Name: " + boss.getName());
         System.out.println("Boss Health: " + boss.getHealth());
         System.out.println("Boss Weapon Type: " + boss.getWeapon().getType());
